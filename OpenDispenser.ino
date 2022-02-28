88639e71d85c19040b7dc2b45909ff5dec07d3da
The90Frank <francescov.90@gmail.com>
2022-02-28 19:15:34 +0100

    sostituita malloc con calloc

--- OpenDispenser.ino
+++ OpenDispenser.ino
@@ -3,7 +3,7 @@
 Valve ** valves;
 
 void setup() {
-  valves = (Valve**)malloc(SIZE*sizeof(Valve*));
+  valves = (Valve**)calloc(SIZE, sizeof(Valve*));
   valves[0] = new Valve(13,4);
   valves[1] = new Valve(12,3);
   valves[2] = new Valve(11,2);
